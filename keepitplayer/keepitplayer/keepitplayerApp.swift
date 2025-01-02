bcd648b1aae5d78a2ad74d1df6c811c7cda3e127
Adrian <110757740+aclrians@users.noreply.github.com>
2025-01-02 01:12:57 -0800

    Initial commit :P

---
+++ keepitplayer/keepitplayer/keepitplayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  keepitplayerApp.swift
+//  keepitplayer
+//
+//  Created by Adrian  on 1/2/25.
+//
+
+import SwiftUI
+
+@main
+struct keepitplayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ keepitplayer/keepitplayerTests/keepitplayerTests.swift
@@ -0,0 +1,17 @@
+//
+//  keepitplayerTests.swift
+//  keepitplayerTests
+//
+//  Created by Adrian  on 1/2/25.
+//
+
+import Testing
+@testable import keepitplayer
+
+struct keepitplayerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
